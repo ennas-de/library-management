4daf97053cdd121a25b0bcdfa46b1bc655fe5675
ennas-de <dev.abdulhakeem@gmail.com>
2023-09-13 15:07:27 +0100

    update controller

--- server/controllers/authorController.js
+++ server/controllers/authorController.js
@@ -1,89 +1,87 @@
-const Author = require('../models/author')
-
+const Author = require("../models/author.js");
 
 //read
 const getAuthor = async (req, res) => {
-    const authorId = req.params.id;
+  const authorId = req.params.id;
 
-    Author.findById(authorId, (err, author) => {
-        if (err) {
-            return res.status(400).json({ success: false, err });
-        }
+  Author.findById(authorId, (err, author) => {
+    if (err) {
+      return res.status(400).json({ success: false, err });
+    }
 
-        return res.status(200).json({
-            success: true,
-            author
-        });
+    return res.status(200).json({
+      success: true,
+      author,
     });
-}
+  });
+};
 
 const getAllAuthors = async (req, res) => {
-    Author.find({}, (err, authors) => {
-        if (err) {
-            return res.status(400).json({ success: false, err });
-        }
+  Author.find({}, (err, authors) => {
+    if (err) {
+      return res.status(400).json({ success: false, err });
+    }
 
-        return res.status(200).json({
-            success: true,
-            authorsList: authors
-        });
-    })
-}
+    return res.status(200).json({
+      success: true,
+      authorsList: authors,
+    });
+  });
+};
 
 //create
 const addAuthor = async (req, res) => {
-    const newAuthor = req.body
+  const newAuthor = req.body;
 
-    Author.create(newAuthor, (err, author) => {
-        if (err) {
-            return res.status(400).json({ success: false, err });
-        }
+  Author.create(newAuthor, (err, author) => {
+    if (err) {
+      return res.status(400).json({ success: false, err });
+    }
 
-        return res.status(200).json({
-            success: true,
-            newAuthor: author
-        });
-    })
-}
+    return res.status(200).json({
+      success: true,
+      newAuthor: author,
+    });
+  });
+};
 
 //update
 const updateAuthor = async (req, res) => {
-    const authorId = req.params.id
-    const updatedAuthor = req.body
+  const authorId = req.params.id;
+  const updatedAuthor = req.body;
 
-    Author.findByIdAndUpdate(authorId, updatedAuthor, (err, author) => {
-        if (err) {
-            return res.status(400).json({ success: false, err });
-        }
+  Author.findByIdAndUpdate(authorId, updatedAuthor, (err, author) => {
+    if (err) {
+      return res.status(400).json({ success: false, err });
+    }
 
-        return res.status(200).json({
-            success: true,
-            updatedAuthor: author
-        });
-    })
-}
-
+    return res.status(200).json({
+      success: true,
+      updatedAuthor: author,
+    });
+  });
+};
 
 //delete
 const deleteAuthor = async (req, res) => {
-    const authorId = req.params.id
+  const authorId = req.params.id;
 
-    Author.findByIdAndDelete(authorId, (err, author) => {
-        if (err) {
-            return res.status(400).json({ success: false, err });
-        }
+  Author.findByIdAndDelete(authorId, (err, author) => {
+    if (err) {
+      return res.status(400).json({ success: false, err });
+    }
 
-        return res.status(200).json({
-            success: true,
-            deletedAuthor: author
-        });
-    })
-}
+    return res.status(200).json({
+      success: true,
+      deletedAuthor: author,
+    });
+  });
+};
 
 module.exports = {
-    getAuthor,
-    getAllAuthors,
-    addAuthor,
-    updateAuthor,
-    deleteAuthor
-}
+  getAuthor,
+  getAllAuthors,
+  addAuthor,
+  updateAuthor,
+  deleteAuthor,
+};
