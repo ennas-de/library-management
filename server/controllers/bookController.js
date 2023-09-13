a7db9196c2c70f0336f73c3fce7f52debe22b1e9
ennas-de <dev.abdulhakeem@gmail.com>
2023-09-13 15:07:36 +0100

    update controller

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,4 +1,4 @@
-const Book = require('../models/book')
+const Book = require("../models/book.js");
 const mongoose = require("mongoose");
 
 const getBook = async (req, res) => {
@@ -6,105 +6,106 @@ const getBook = async (req, res) => {
 
   Book.findById(bookId, (err, book) => {
     if (err) {
-      return res.status(400).json({success: false, err});
+      return res.status(400).json({ success: false, err });
     }
 
     return res.status(200).json({
       success: true,
-      book
+      book,
     });
   });
-}
+};
 
 const getAllBooks = async (req, res) => {
-  Book.aggregate([{
-    $lookup: {
-      from: "authors",
-      localField: "authorId",
-      foreignField: "_id",
-      as: "author"
-    },
-  },
+  Book.aggregate([
     {
-      $unwind: "$author"
+      $lookup: {
+        from: "authors",
+        localField: "authorId",
+        foreignField: "_id",
+        as: "author",
+      },
+    },
+    {
+      $unwind: "$author",
     },
     {
       $lookup: {
         from: "genres",
         localField: "genreId",
         foreignField: "_id",
-        as: "genre"
+        as: "genre",
       },
-
     },
     {
-      $unwind: "$genre"
-    },]).exec((err, books) => {
+      $unwind: "$genre",
+    },
+  ]).exec((err, books) => {
     if (err) {
-      return res.status(400).json({success: false, err});
+      return res.status(400).json({ success: false, err });
     }
 
     return res.status(200).json({
       success: true,
-      booksList: books
+      booksList: books,
     });
   });
-}
+};
 
 const addBook = async (req, res) => {
   const newBook = {
     ...req.body,
     genreId: mongoose.Types.ObjectId(req.body.genreId),
-    authorId: mongoose.Types.ObjectId(req.body.authorId)
-  }
-  console.log(newBook)
+    authorId: mongoose.Types.ObjectId(req.body.authorId),
+  };
+  console.log(newBook);
   Book.create(newBook, (err, book) => {
     if (err) {
-      return res.status(400).json({success: false, err});
+      return res.status(400).json({ success: false, err });
     }
 
     return res.status(200).json({
       success: true,
-      newBook: book
+      newBook: book,
     });
-  })
-}
+  });
+};
 
 const updateBook = async (req, res) => {
-  const bookId = req.params.id
-  const updatedBook = req.body
+  const bookId = req.params.id;
+  const updatedBook = req.body;
 
   Book.findByIdAndUpdate(bookId, updatedBook, (err, book) => {
     if (err) {
-      return res.status(400).json({success: false, err});
+      return res.status(400).json({ success: false, err });
     }
 
     return res.status(200).json({
       success: true,
-      updatedBook: book
+      updatedBook: book,
     });
-  })
-}
+  });
+};
 
 const deleteBook = async (req, res) => {
-  const bookId = req.params.id
+  const bookId = req.params.id;
 
   Book.findByIdAndDelete(bookId, (err, book) => {
     if (err) {
-      return res.status(400).json({success: false, err});
+      return res.status(400).json({ success: false, err });
     }
 
     return res.status(200).json({
       success: true,
-      deletedBook: book
+      deletedBook: book,
     });
-  })
-}
+  });
+};
 
 module.exports = {
   getBook,
   getAllBooks,
   addBook,
   updateBook,
-  deleteBook
-}
+  deleteBook,
+};
