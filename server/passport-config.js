1dfa828156d19e611a20aa7f2a3d2ffb9a484d13
ennas-de <dev.abdulhakeem@gmail.com>
2023-09-13 15:09:37 +0100

    update passport-config.js

--- server/passport-config.js
+++ server/passport-config.js
@@ -1,23 +1,23 @@
 const LocalStrategy = require("passport-local");
-const User = require("./models/user");
+const User = require("./models/user.js");
 
 const initializePassport = (passport) => {
   const authenticateUser = (email, password, cb) => {
-    User.findOne({email: email}, (err, user) => {
+    User.findOne({ email: email }, (err, user) => {
       if (err) {
         return cb(err);
       }
       if (!user) {
-        return cb(null, false, {message: "User not found"});
+        return cb(null, false, { message: "User not found" });
       }
       if (!user.isValidPassword(password)) {
-        return cb(null, false, {message: "Password incorrect"});
+        return cb(null, false, { message: "Password incorrect" });
       } else {
         return cb(null, user);
       }
     });
   };
-  passport.use(new LocalStrategy({usernameField: "email"}, authenticateUser));
+  passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
   passport.serializeUser((user, done) => done(null, user));
   passport.deserializeUser((user, done) => {
     User.findById(user._id, (err, user) => {
